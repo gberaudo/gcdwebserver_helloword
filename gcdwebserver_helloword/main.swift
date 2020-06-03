db5c74fb0221cb361dbbfdf438c10c856d40dc8e
Frédéric Junod <frederic.junod@camptocamp.com>
2020-06-03 16:22:03 +0200

    Initial Commit

---
+++ gcdwebserver_helloword/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gcdwebserver_helloword
+//
+//  Created by Camptocamp on 03.06.20.
+//  Copyright © 2020 Camptocamp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
